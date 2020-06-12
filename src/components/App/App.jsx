75ed5a2edf7a99d9d48377cda496830d7f119836
JoLeaper <joleap123@gmail.com>
2020-06-12 10:47:04 -0700

    installed packages and created seed data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
+import baseBallData from '../../../public/data.js';
 
 export default function App() {
+  const [baseBallData, setBaseBallData] = useState(baseBallData);
   return <h1>Hello World</h1>;
 }
-  
